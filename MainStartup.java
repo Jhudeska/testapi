b3132c2d2be27464e3d303229339a9577baf2a75
Jhudeska <jvsignacia@live.nl>
2020-08-29 13:20:18 +0200

    Classes

--- MainStartup.java
+++ MainStartup.java
@@ -1,42 +1,18 @@
-import java.util.ArrayList;
-
 class MainStartup
         {
         public static void main(String[] args)      
                 {
                 System.out.println("Program began.");
 
-                // Basic arrays. Don't use them. 
-                // You MUST initialize the array with it's values to use it. Lame.
-                int[] myArray = { 0, 1, 2 };
-                myArray[0] = 5;
-                System.out.println(myArray[0]);
+                srcMathLibrary math = new srcMathLibrary();
+                math.PrintFaveNum();
+                math.SetFaveNum(200);
+                math.PrintFaveNum();
+                int addition = math.Add(2, 2);
+                System.out.println(addition);
                 
-                        
-                // Arraylists. These are our basic arrays.
-                ArrayList<Integer> myList = new ArrayList<Integer>();
-                
-                // Add the numbers 0, 1, ..., 9 to the list.
-                for(int i = 0; i < 10; i++)
-                        {
-                        myList.add(i);
-                        }
-                
-                // Access the numbers and print them out.
-                int n = myList.size();
-                for(int i = 0; i < n; i++)
-                        {
-                        int num = myList.get(i);
-                        System.out.print(num + " ");
-                        }
-                System.out.println();
-                        
-                // Other types.
-                ArrayList<Long> longList;
-                ArrayList<Double> doubleList;
-                ArrayList<Boolean> boolList;
-                ArrayList<Character> charList;
-                ArrayList<String> stringList;
+                srcMathLibrary anothermath = new srcMathLibrary(-1);
+                anothermath.PrintFaveNum();
 
                 System.out.println("Program ended.");
                 }
